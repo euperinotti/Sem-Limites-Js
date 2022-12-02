a678e31670db687f21c0933e02c5b6b750651d92
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-12-02 09:18:28 -0300

    terminando os models

--- src/models/Client.ts
+++ src/models/Client.ts
@@ -1,18 +1,100 @@
-interface IClient {
-    name: string;
-    lastName: string;
+import { Model, DataTypes } from "sequelize";
+import { componentSequelize } from "../instances/mysql";
+
+type AccountType = 'A' | 'C';
+type PixTypeKey = 'Telefone' | 'CPF' | 'E-mail'
+
+export class Client extends Model {
+    nome: string;
+    sobrenome: string;
     email: string;
-    password: string;
     cpf: string;
-    ddd: string;
-    phone: string;
-    address: {
-        street: string;
-        district: string;
-        houseNumber: number;
+    telefone: {
+        ddd: string;
+        numero: string;
+    }
+    senha: string;
+    endereco: {
+        rua: string;
+        numeroDaCasa: number;
+        bairro: string
     };
     pix: {
-        type: 'telefone' | 'cpf' | 'email';
-        key: 'string';
+        tipoDeChave: PixTypeKey;
+        chave: string;
     };
+    tipo: AccountType;
+
+    constructor(nome: string, sobrenome: string, email: string, cpf: string, telefone: {ddd: string, numero: string}, senha: string, endereco: {rua: string, numeroDaCasa: number, bairro: string}, pix: {tipoDeChave: PixTypeKey, chave: string}, tipo: AccountType){
+        super();
+
+        this.nome = nome.trim().slice(0,45);
+        this.sobrenome = sobrenome.trim().slice(0,45);
+        this.email = email
+    }
 }
+
+export const Usuario = componentSequelize.define<Client>('Usuario', {
+    idcliente: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    frete: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'frete',
+            key: 'idfrete'
+        }
+    },
+    nome: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+    },
+    sobrenome: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(70),
+        allowNull: false
+    },
+    cpf: {
+        type: DataTypes.STRING(14),
+        allowNull: false
+    },
+    ddd: {
+        type: DataTypes.STRING(3),
+        allowNull: false
+    },
+    telefone: {
+        type: DataTypes.STRING(10),
+        allowNull: false
+    },
+    senha: {
+        type: DataTypes.STRING(32),
+        allowNull: false
+    },
+    rua: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    n_casa: {
+        type: DataTypes.MEDIUMINT,
+        allowNull: false
+    },
+    pix: {
+        type: DataTypes.STRING(50),
+        allowNull: true
+    },
+    tipo: {
+        type: DataTypes.CHAR(1),
+        allowNull: false
+    }
+
+}, {
+    tableName: 'usuario',
+    timestamps: false
+})
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,4 +1,4 @@
-import { Model } from "sequelize";
+import { Model, DataTypes, Op } from "sequelize";
 import { componentSequelize } from "../instances/mysql";
 
 type ProductCategory = 'Camiseta' | 'Calça' | 'Bermuda' | 'Tênis' | 'Jaqueta' | 'Moletom';
@@ -6,37 +6,79 @@ type ProductSize = '36' | '37' | '38' | '39' | '40' | '41' | 'P' | 'M' | 'G' | '
 
 export class Product extends Model {
     nome: string;
-    categoria: ProductCategory;
+    categoria: number;
     tamanho: ProductSize;
     preco: number;
-    imagem?: string;
     cor: string;
     estoque: number;
+    imagem?: string;
     descricao?: string;
     desconto?: number;
 
-    constructor(nome: string, categoria:ProductCategory, tamanho: ProductSize, preco:number, cor: string, estoque: number, imagem?:string, descricao?: string, desconto?: number){
+    constructor(nome: string, categoria:number, tamanho: ProductSize, preco:number, cor: string, estoque: number, imagem?:string, descricao?: string, desconto?: number){
         super();
 
-        this.nome = nome;
+        this.nome = nome.trim().slice(0,60);
         this.categoria = categoria;
         this.tamanho = tamanho;
-        this.preco = preco;
+        this.preco = parseFloat(`${preco}`);
         this.cor = cor;
         this.estoque = estoque;
-        if(imagem){
-            this.imagem = imagem;
-        }
-        if(descricao){
-            this.descricao = descricao;
-        }
-        if(desconto){
-            this.desconto = desconto;
-        }
+        this.imagem ? imagem : null
+        this.descricao ? descricao : null;
+        this.desconto ? parseFloat(`${desconto}`) : null;
+        
     }
 }
 
-export const Produto = componentSequelize.define<Product>('produto',
-
-
-)
+export const Produto = componentSequelize.define<Product>('Produto',{
+    idproduto: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    categoria: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'categoria',
+            key: 'idcategoria'
+        }
+    },
+    nome: {
+        type: DataTypes.STRING(60),
+        allowNull: false,
+    },
+    tamanho: {
+        type: DataTypes.STRING(2),
+        allowNull: false
+    },
+    preco: {
+        type: DataTypes.FLOAT(7,2),
+        allowNull: false
+    },
+    Imagem: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    cor: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+    Estoque: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    descricao: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    desconto: {
+        type: DataTypes.FLOAT(7,2),
+        allowNull: true
+    }
+}, {
+    tableName: 'produto',
+    timestamps: false
+})
